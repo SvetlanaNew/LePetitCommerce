d178e36a465b2fb6341aa322129f11f2217d636a
Gaiden92i <samy_fouchal@hotmail.fr>
2019-08-13 20:51:12 +0200

    test

--- js/script.js
+++ js/script.js
@@ -232,39 +232,6 @@ function resultat5(){
 };
 
 
-/*********************Menu accordéon Description fiche produit***************************/
-
-
-$(document).ready(function(){
-  $('#barre').click(function(){
-        
-        $('#rideau').slideToggle(500);
-     });
-
-    $('#barre2').click(function(){
-        
-        $('#rideau2').slideToggle(500);
-     });
- 
-    $('#barre3').click(function(){
-        
-        $('#rideau3').slideToggle(500);
-     });
-   
-    $('#barre4').click(function(){
-        
-        $('#rideau4').slideToggle(500);
-     });
-    });
-
-    /**********************************Récupération photo fiche produit ************************************/
-    function recupImg(){
-      listimg = document.getElementById('photoFicheProduit').getElementsByTagName('img');
-    
-      for(var i = 0; i < listimg.length; i++){
-         alert(listimg[i].src);
-      }
-   }
 
 
 
