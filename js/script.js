35fc30f3caa9c17940274f5704c030f6cf7cba17
Svetlana <kikcecka@hotmail.com>
2019-08-13 19:02:36 +0200

    probleme de lien ds index

--- js/script.js
+++ js/script.js
@@ -294,27 +294,7 @@ function resultat6(){
 /*********************Menu accordéon Description fiche produit***************************/
 
 
-$(document).ready(function(){
-  $('#barre').click(function(){
-        
-        $('#rideau').slideToggle(500);
-     });
 
-    $('#barre2').click(function(){
-        
-        $('#rideau2').slideToggle(500);
-     });
- 
-    $('#barre3').click(function(){
-        
-        $('#rideau3').slideToggle(500);
-     });
-   
-    $('#barre4').click(function(){
-        
-        $('#rideau4').slideToggle(500);
-     });
-    });
 
     /**********************************Récupération photo fiche produit ************************************/
     function recupImg(){
