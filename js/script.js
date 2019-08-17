62c9c259b7408f39c6f20379d55f6e4b70c5f83f
Gaiden92i <samy_fouchal@hotmail.fr>
2019-08-18 00:14:15 +0200

    page chemises

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){
+$('.barre').click(function(){
+     
+$('.rideau').slideToggle(500);
+  });
+
+ $('.barre2').click(function(){
+     
+     $('.rideau2').slideToggle(500);
+  });
+
+ $('.barre3').click(function(){
+     
+     $('.rideau3').slideToggle(500);
+  });
+
+ $('.barre4').click(function(){
+     
+     $('.rideau4').slideToggle(500);
+  });
+
+ });
